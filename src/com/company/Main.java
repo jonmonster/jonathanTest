8fe03ab830a3fef6436c9989c372e0152bb2e618
coreygarcia <corey.garcia.229@gmail.com>
2017-03-23 21:59:14 -0700

    Assignment #1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,47 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello Jon!");
+
+        /**
+         * Assignment #1
+         * Create a method that looks at the below lists (listOne and listTwo), and finds the numbers that exists in both of the lists
+         * Add this method to the JonathanUtil class (I just added this class)
+         * For example, both lists have the number 1 in them.  Your method should find the number 1.
+         * Then print these numbers to the console
+         */
+
+        ArrayList<Integer> listOne = new ArrayList<Integer>();
+        ArrayList<Integer> listTwo = new ArrayList<Integer>();
+
+        listOne.add(1);
+        listOne.add(3);
+        listOne.add(4);
+        listOne.add(6);
+        listOne.add(23);
+        listOne.add(56);
+        listOne.add(47);
+        listOne.add(38);
+        listOne.add(96);
+        listOne.add(54);
+        listOne.add(53);
+        listOne.add(11);
+        listOne.add(77);
+
+        listTwo.add(1);
+        listTwo.add(65);
+        listTwo.add(54);
+        listTwo.add(78);
+        listTwo.add(79);
+        listTwo.add(96);
+        listTwo.add(44);
+        listTwo.add(32);
+        listTwo.add(24);
+        listTwo.add(21);
+
+        System.out.print("The duplicated numbers in the lists are..");
     }
 }
