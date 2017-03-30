afedc96c909c18a2c52ebd8b4c2de837b7b7dc0b
jonmonster <sl.jonwong@gmail.com>
2017-03-29 17:11:41 -0700

    Added findDupes function to JonathanUtil

--- src/com/company/JonathanUtil.java
+++ src/com/company/JonathanUtil.java
@@ -1,7 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class JonathanUtil {
 
     public JonathanUtil() { }
+
+    public ArrayList<Integer> findDupes(ArrayList<Integer> a, ArrayList<Integer>b){
+        ArrayList<Integer> results = new ArrayList<Integer>();
+
+        for(int i=0; i< a.size(); i++){
+            for(int j=0; j<b.size(); j++){
+                if(a.get(i) == b.get(j)){
+                    results.add(a.get(i));
+                }
+            }
+        }
+
+        return results;
+    }
 
 }
